695f048d564bea483a086de8ae2e55d05f71f94f
Tech-a50 <tech-a50@TECH-A50s-MacBook-Air.local>
2019-04-22 15:08:11 +0100

    added aliens block position variables

--- js/app.js
+++ js/app.js
@@ -1,29 +1,46 @@
 $(function(){
+  var gamerunning = false;
   // Target container
   var container = $(".container");
   // Target ship
   var ship = $(".ship");
-  var shipwidth = ship.width();
   // Initial ship postion
   var shipx = 450;
   ship.css({
     'left': shipx
   })
+  // Target aliens
+  var aliens = $(".aliens");
+  // Initial alien position
+  var aliensx = 170;
+  var aliensy = 100;
+  aliens.css({
+    'left': aliensx,
+    'top': aliensy
+  })
 
+$(".start-button").click(function(){
+
+
+})
+
+
+var containerLeft = container.offset().left;
+var containerRight = containerLeft + container.width();
+
+// ============== Control of ship ============================
 $("body").keydown(function(){
   // coordinates of container walls
-  var containerLeft = container.offset().left;
-  var containerRight = containerLeft + container.width();
   // coordinates of ship walls
   var shipLeft = ship.offset().left;
   var shipRight = shipLeft + ship.width();
 
   if ((event.key == "a" || event.key == "ArrowLeft") && (shipLeft>containerLeft)) {
-    console.log("dog");
+    console.log("left");
     console.log(shipx);
     shipx-=50;
-  } else if ((event.key == "d" || event.key == "ArrowRight") && (shipRight<containerRight)) {
-    console.log("cat");
+    } else if ((event.key == "d" || event.key == "ArrowRight") && (shipRight<containerRight)) {
+    console.log("right");
     console.log(containerRight);
     shipx+=50;
   }
@@ -31,6 +48,5 @@ $("body").keydown(function(){
     'left': shipx+'px'
   })
 })
-
 
 })
